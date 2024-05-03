e205f4c9d5c8b9a291825d85afc3ca3901e89195
itsjackie00 <jackiedanso33@gmail.com>
2024-05-03 03:41:13 +0200

    added directory table.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 include __DIR__ . '/Views/header.php';
 ?>
+<main class="container">
+<?php
+include __DIR__  . '/Views/table.php';
+?>
+</main>
 
 <?php
 include __DIR__ . '/Views/footer.php';
