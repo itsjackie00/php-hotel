4687f522869926a7d0ffd1ab2fe1ec271d81abdf
itsjackie00 <jackiedanso33@gmail.com>
2024-05-03 03:35:34 +0200

    added include header

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- BOOTSTRAP -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    
-    <title>PHP Hotel</title>
-</head>
-<body>
-    <header>
-        <h1>PHP Hotel</h1>
-    </header>
+<?php
+include __DIR__ . '/Views/header.php';
+?>
+
 </body>
 </html>
