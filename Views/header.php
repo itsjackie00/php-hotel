9ad6c4c447bef959fd66511d0b7df905979c2802
itsjackie00 <jackiedanso33@gmail.com>
2024-05-03 03:42:12 +0200

    ADDED FONT AWASOME LINk

--- Views/header.php
+++ Views/header.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- CSS -->
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
     
     <title>PHP Hotel</title>
 </head>
