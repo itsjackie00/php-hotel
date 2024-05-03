cb26e45f9f301ad780565929c2d2e70fd6af61a1
itsjackie00 <jackiedanso33@gmail.com>
2024-05-03 05:17:35 +0200

    added foreach on hotel

--- index.php
+++ index.php
@@ -1,10 +1,47 @@
 <?php
+include __DIR__ . '/Models/steam.php';
+
+if (isset($GET['parking'])) {
+    foreach ($hotels as $hotel) {
+        if ($parking == '1') {
+            if ($hotel['parking'] == true) {
+                $template .= '<tr>';
+                $template .= '<td>' . $hotel['name'] . '</td>';
+                $template .= '<td>' . $hotel['description'] . '</td>';
+                $template .= '<td>' . $hotel['parking'] . '</td>';
+                $template .= '<td>' . $hotel['vote'] . '</td>';
+                $template .= '<td>' . $hotel['distance_to_center'] . '</td>';
+                $template .= '</tr>';
+            }
+        } elseif ($parking == '0') {
+            if ($hotel['parking'] == false) {
+                $template .= '<tr>';
+                $template .= '<td>' . $hotel['name'] . '</td>';
+                $template .= '<td>' . $hotel['description'] . '</td>';
+                $template .= '<td>' . $hotel['parking'] . '</td>';
+                $template .= '<td>' . $hotel['vote'] . '</td>';
+                $template .= '<td>' . $hotel['distance_to_center'] . '</td>';
+                $template .= '</tr>';
+            }
+        }else {
+            $template .= '<tr>';
+                $template .= '<td>' . $hotel['name'] . '</td>';
+                $template .= '<td>' . $hotel['description'] . '</td>';
+                $template .= '<td>' . $hotel['parking'] . '</td>';
+                $template .= '<td>' . $hotel['vote'] . '</td>';
+                $template .= '<td>' . $hotel['distance_to_center'] . '</td>';
+                $template .= '</tr>';
+        }
+    }
+}
+?>
+<?php
 include __DIR__ . '/Views/header.php';
 ?>
 <main class="container">
-<?php
-include __DIR__  . '/Views/table.php';
-?>
+    <?php
+    include __DIR__  . '/Views/table.php';
+    ?>
 </main>
 
 <?php
