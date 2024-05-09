6dc540e5e3765cf761895da44ccb356c4c058672
itsjackie00 <jackiedanso33@gmail.com>
2024-05-09 10:11:15 +0200

    added user array

---
+++ Models/users.php
@@ -0,0 +1,38 @@
+<?php
+$users = [
+    [
+        'id' => 1,
+        'name' => 'Stefano',
+        'surname' => 'Stefani',
+        'password' => '12345',
+        'email' => 'stefano@stefano.com',
+    ],
+    [
+        'id' => 2,
+        'name' => 'Jessica',
+        'surname' => 'Luciani',
+        'password' => '12345',
+        'email' => 'jessica@jessica.com',
+    ],
+    [
+        'id' => 3,
+        'name' => 'Lorenzo',
+        'surname' => 'Lorenzi',
+        'password' => '12345',
+        'email' => 'lorenzo@lorenzo.com',
+    ],
+    [
+        'id' => 4,
+        'name' => 'Alessandra',
+        'surname' => 'Alessandrini',
+        'password' => '112233',
+        'email' => 'alessandra@alessandra.com',
+    ],
+    [
+        'id' => 5,
+        'name' => 'Admin',
+        'surname' => 'Admin',
+        'password' => '11111',
+        'email' => 'admin@steam.com',
+    ]
+];
